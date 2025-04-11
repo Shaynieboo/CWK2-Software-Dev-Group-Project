aafe1bff85b7b75f4b5be3749c8c6bd6f7340504
Shayne <shaynefelicienbrown@gmail.com>
2025-04-11 17:34:14 +0100

    Added signup view, form, and HTML template

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,7 @@ def signup_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')  # or redirect to home page
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
